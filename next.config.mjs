b3e6cae118024bd1f0bf3642030af54157174396
Rishad Omar <rishad@aux.co.za>
2024-10-11 15:00:25 +0200

    Privacy page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+        domains: ['app.cvbuilder.co.za']
+    }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
         <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
             <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
                 <Image
-                    className='dark:invert'
-                    src='https://nextjs.org/icons/next.svg'
-                    alt='Next.js logo'
+                    //className='dark:invert'
+                    src='https://app.cvbuilder.co.za/logo-pencil.jpeg'
+                    alt='CV Builder logo'
                     width={180}
                     height={38}
                     priority
