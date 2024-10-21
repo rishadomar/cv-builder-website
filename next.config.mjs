207860e34259b1939b166b5f402510646464e457
Rishad Omar <rishad@aux.co.za>
2024-10-21 19:09:00 +0200

    Was calling useEffect twice

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false, // Add this line to disable React Strict Mode
     output: 'export',
     trailingSlash: true,
     images: {
