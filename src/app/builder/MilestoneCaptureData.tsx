1f2a62a7c5922cf8123cd5e5f17678fb5bf74585
Rishad Omar <rishad@aux.co.za>
2024-11-02 14:23:03 +0200

    Instead of useEffect relying on the fieldValues - as recommended by AI

--- src/app/builder/MilestoneCaptureData.tsx
+++ src/app/builder/MilestoneCaptureData.tsx
@@ -1,10 +1,10 @@
 import { useAppSelector } from '@/lib/store/hooks';
 import { PaymentComplete } from '@/app/builder/PaymentComplete';
-import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const PaymentPrompt = dynamic(() => import('@/components/PaymentPrompt').then((mod) => mod.PaymentPrompt), {
-    ssr: false
+    ssr: false,
+    loading: () => <div>Loading...</div>
 });
 
 type MilestoneCaptureDataProps = {
@@ -13,20 +13,10 @@ type MilestoneCaptureDataProps = {
 };
 
 export function MilestoneCaptureData({ onNext, onPrevious }: MilestoneCaptureDataProps) {
-    const [isClient, setIsClient] = useState(false);
     const fieldValues = useAppSelector((state) => state.fieldValues);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    console.log('Payment in fieldValues', fieldValues.payment);
 
     if (fieldValues.payment) {
         return <PaymentComplete paymentDetails={fieldValues.payment} onNext={onNext} onPrevious={onPrevious} />;
-    } else if (!isClient) {
-        return null;
-    } else {
-        return <PaymentPrompt onNext={onNext} onPrevious={onPrevious} />;
     }
+    return <PaymentPrompt onNext={onNext} onPrevious={onPrevious} />;
 }
