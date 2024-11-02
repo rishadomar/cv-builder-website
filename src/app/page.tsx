49cae37615d81807eadb67f2b227643454f3fe0f
Rishad Omar <rishad@aux.co.za>
2024-11-02 15:24:38 +0200

    Adding favicons although they don't display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
     title: 'CV Builder using AI!',
     description: 'Create your professional CV easily with our AI-powered CV builder.',
-    robots: 'noindex, nofollow'
+    robots: 'noindex, nofollow',
+    icons: {
+        icon: '/images/favicon.ico'
+    }
 };
 
 export default function Home() {
