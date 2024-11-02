96dc3ddf0d5180e69a058a80110bf72aaa60c2c5
Rishad Omar <rishad@aux.co.za>
2024-11-02 13:23:54 +0200

    Refactor to load Paystack button dynamically because getting the error: "window is not defined" when building the app

--- src/app/builder/MilestoneCaptureData.tsx
+++ src/app/builder/MilestoneCaptureData.tsx
@@ -1,6 +1,11 @@
 import { useAppSelector } from '@/lib/store/hooks';
-import { PaymentPrompt } from '@/app/builder/PaymentPrompt';
 import { PaymentComplete } from '@/app/builder/PaymentComplete';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const PaymentPrompt = dynamic(() => import('@/components/PaymentPrompt').then((mod) => mod.PaymentPrompt), {
+    ssr: false
+});
 
 type MilestoneCaptureDataProps = {
     onNext: () => void;
@@ -8,12 +13,19 @@ type MilestoneCaptureDataProps = {
 };
 
 export function MilestoneCaptureData({ onNext, onPrevious }: MilestoneCaptureDataProps) {
+    const [isClient, setIsClient] = useState(false);
     const fieldValues = useAppSelector((state) => state.fieldValues);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
     console.log('Payment in fieldValues', fieldValues.payment);
 
     if (fieldValues.payment) {
         return <PaymentComplete paymentDetails={fieldValues.payment} onNext={onNext} onPrevious={onPrevious} />;
+    } else if (!isClient) {
+        return null;
     } else {
         return <PaymentPrompt onNext={onNext} onPrevious={onPrevious} />;
     }
