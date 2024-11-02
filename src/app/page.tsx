b396133079feea6199831543be6045229047fb3e
Rishad Omar <rishad@aux.co.za>
2024-11-02 15:28:52 +0200

    Fixing metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Welcome from '@/app/Welcome';
 import type { Metadata } from 'next';
 
@@ -14,13 +13,6 @@ export const metadata: Metadata = {
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>CV Builder</title>
-                <meta name='description' content='Create your professional CV easily with our AI-powered CV builder.' />
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <meta name='robots' content='noindex, nofollow' />
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
             <Welcome />
         </>
     );
