842fc5b2e15f4ace2af0243211b22afa4db657d0
Rishad Omar <rishad@aux.co.za>
2024-11-16 07:28:12 +0200

    An exampl eof a page that requires authentication

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -1,5 +1,10 @@
+// src/app/builder/page.tsx
+'use client';
 import ProgressSteps from '@/components/ProgressSteps';
+import withAuth from '@/components/withAuth';
 
-export default function BuilderPage() {
+const BuilderPage: React.FC = () => {
     return <ProgressSteps />;
-}
+};
+
+export default withAuth(BuilderPage);
---
+++ src/components/withAuth.tsx
@@ -0,0 +1,28 @@
+// src/components/withAuth.tsx
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useAppSelector } from '@/lib/store/hooks';
+import { selectIsLoggedIn } from '@/lib/store/authentication/authenticationSlice';
+
+const withAuth = (WrappedComponent: React.ComponentType) => {
+    const AuthenticatedComponent: React.FC = (props) => {
+        const isLoggedIn = useAppSelector(selectIsLoggedIn);
+        const router = useRouter();
+
+        React.useEffect(() => {
+            if (!isLoggedIn) {
+                router.push('/authentication/signin');
+            }
+        }, [isLoggedIn, router]);
+
+        if (!isLoggedIn) {
+            return null; // or a loading spinner
+        }
+
+        return <WrappedComponent {...props} />;
+    };
+
+    return AuthenticatedComponent;
+};
+
+export default withAuth;
