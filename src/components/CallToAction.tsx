fdeac4d856ed4a5fccd9160ae1c504b1b5902840
Rishad Omar <rishad@aux.co.za>
2024-11-19 10:39:41 +0200

    Added CallToAction as client component.

---
+++ src/components/CallToAction.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import { ArrowRight } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+interface CallToActionProps {
+    label: string;
+    color: 'blue' | 'white';
+    withArrow: boolean;
+}
+
+const CallToAction = ({ label, color, withArrow }: CallToActionProps) => {
+    const router = useRouter();
+
+    const callToAction = () => {
+        router.push('/authentication');
+    };
+
+    return (
+        <button
+            className={`${
+                color === 'white'
+                    ? 'bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition flex items-center'
+                    : 'bg-white text-blue-600 px-6 md:px-8 py-3 md:py-4 rounded-lg hover:bg-gray-100 transition'
+            }`}
+            onClick={() => callToAction()}
+        >
+            {label}
+            {withArrow && <ArrowRight className='ml-2' />}
+        </button>
+    );
+};
+export default CallToAction;
