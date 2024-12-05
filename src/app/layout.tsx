d25206b76b02bdc881fa60bbcbfededeaa1adae5
Rishad Omar <rishad@aux.co.za>
2024-12-05 16:49:37 +0200

    Add Toast library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { inter } from './fonts';
 import StoreProvider from '@/lib/store/StoreProvider';
 import './globals.css';
 import CookieLoader from '@/components/CookieLoader';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import type { Metadata } from 'next';
 import Navbar from '@/components/Navbar';
 
@@ -23,6 +25,7 @@ export default function RootLayout({
         <html lang='en'>
             <body className={inter.className}>
                 <StoreProvider>
+                    <ToastContainer />
                     <CookieLoader />
                     <div className='min-h-screen flex flex-col'>
                         <Navbar />
