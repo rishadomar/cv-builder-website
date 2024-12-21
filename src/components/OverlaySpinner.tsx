9a1772aa69506180af4327fcbb4f4fead951af88
Rishad Omar <rishad@aux.co.za>
2024-12-21 14:49:33 +0200

    No blurring effect

--- src/components/OverlaySpinner.tsx
+++ src/components/OverlaySpinner.tsx
@@ -1,7 +1,7 @@
 import { Loader } from 'lucide-react';
 
 export const OverlaySpinner = () => (
-    <div className='fixed inset-0 flex items-center justify-center bg-black/20 backdrop-blur-sm z-50'>
+    <div className='fixed inset-0 flex items-center justify-center bg-black/70 z-50'>
         <Loader className='h-10 w-10 animate-spin text-white' />
     </div>
 );
