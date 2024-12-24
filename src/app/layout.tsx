e5e702551973b130de3f7d0a6cf33b43a9125b7b
Rishad Omar <rishad@aux.co.za>
2024-12-24 09:13:08 +0200

    New favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,51 @@ export const metadata: Metadata = {
     description: 'Create your professional CV easily with our AI-powered CV builder.',
     robots: 'noindex, nofollow',
     icons: {
-        icon: '/images/favicon.ico'
+        icon: [
+            {
+                url: '/favicon.ico',
+                sizes: 'any'
+            },
+            {
+                url: '/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png'
+            },
+            {
+                url: '/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png'
+            }
+        ],
+        apple: {
+            url: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+        },
+        other: [
+            {
+                rel: 'android-chrome',
+                url: '/android-chrome-192x192.png',
+                sizes: '192x192'
+            },
+            {
+                rel: 'android-chrome',
+                url: '/android-chrome-512x512.png',
+                sizes: '512x512'
+            }
+        ]
     }
 };
+
+{
+    /* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest"></link> 
+https://favicon.io/logo-generator/
+
+*/
+}
 
 export default function RootLayout({
     children
