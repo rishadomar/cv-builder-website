c3ec355536fc63774468ea96073a34438fb958c7
Rishad Omar <rishad@aux.co.za>
2024-12-27 16:45:44 +0200

    Use route instead of page

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,32 @@
+// app/sitemap.xml/route.ts
+const EXTERNAL_DATA_URL = 'https://cvbuilder.co.za';
+
+function generateSiteMap(pages: string[]) {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlSet xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${EXTERNAL_DATA_URL}</loc>
+     </url>
+     ${pages
+         .map(
+             (page) => `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}${page}`}</loc>
+       </url>
+     `
+         )
+         .join('')}
+   </urlSet>`;
+}
+
+export async function GET() {
+    const pages = ['/faqs', '/privacy', '/terms'];
+
+    const sitemap = generateSiteMap(pages);
+
+    return new Response(sitemap, {
+        headers: {
+            'Content-Type': 'text/xml'
+        }
+    });
+}
