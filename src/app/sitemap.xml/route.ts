45b9548259d685fbf8a6443ec935206fe00b0276
Rishad Omar <rishad@aux.co.za>
2025-01-17 11:05:57 +0200

    Fixed sitemap. Google was complaining that it's invalid.

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -3,7 +3,7 @@ const EXTERNAL_DATA_URL = 'https://cvbuilder.co.za';
 
 function generateSiteMap(pages: string[]) {
     return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlSet xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>${EXTERNAL_DATA_URL}</loc>
      </url>
