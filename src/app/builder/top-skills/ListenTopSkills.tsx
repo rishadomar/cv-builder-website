85f8f0e0e558a4afa8553103b40cd20bf1643158
Rishad Omar <rishad@aux.co.za>
2025-03-15 15:19:43 +0200

    Neaten up

--- src/app/builder/top-skills/ListenTopSkills.tsx
+++ src/app/builder/top-skills/ListenTopSkills.tsx
@@ -8,7 +8,8 @@ type ListenTopSkillsProps = {
 export const ListenTopSkills = ({ dateGenerated }: ListenTopSkillsProps) => {
     return (
         <div className='border border-gray-200 rounded-lg p-4 mt-4'>
-            <div className='text-xs'>{`Listen to your topskills. Generated on: ${formatRelativeDateTime(
+            <div className='text-xs'>Listen to the discussion about your top skills</div>
+            <div className='text-xs text-gray-400 mt-1'>{`Generated on: ${formatRelativeDateTime(
                 new Date(dateGenerated)
             )}`}</div>
             <PreSignedAudioPlayer className='mt-4' />
